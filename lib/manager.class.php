2b2614cdb539a880d648c8e5fd036005e24fbd9e
Sukhtseren Tsetsegee <sukhtseren.tsetsegee@cs-soft.cz>
2023-02-27 16:53:18 +0100

    Update

--- lib/manager.class.php
+++ lib/manager.class.php
@@ -26,17 +26,37 @@ class Manager
             if (is_array($items)) {
                 $matched = array_filter($items, function ($item) use ($queryParams) {
                     foreach ($queryParams as $key => $value) {
-                        if (isset($item[$key]) && !str_contains($item[$key], $value)) {
-                            return false;
+                        if (isset($item[$key])) {
+                            $itemValue = strtolower(self::RM_DIACRITICS($item[$key]));
+                            $searchValue = strtolower(self::RM_DIACRITICS($value));
+                            if (!str_contains($itemValue, $searchValue)) {
+                                return false;
+                            }
                         }
                     }
                     return true;
                 });
+                $matched = array_values($matched);
 
                 // nakonec vrati vyfiltrovane zaznamy
-                $result = count($matched) ? json_encode($matched) : $result;
+                $result = count($matched) ? json_encode($matched, false) : $result;
             }
         }
         return $result;
+    }
+
+    static function RM_DIACRITICS(string $value)
+    {
+        $table = array(
+            ' ' => '-', 'Š' => 'S', 'š' => 's', 'Ð' => 'Dj', 'Ž' => 'Z', 'ž' => 'z', 'C' => 'C', 'c' => 'c', 'C' => 'C', 'c' => 'c',
+            'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E',
+            'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O',
+            'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss',
+            'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c', 'è' => 'e', 'é' => 'e',
+            'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o',
+            'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ý' => 'y', 'ý' => 'y', 'þ' => 'b',
+            'ÿ' => 'y', 'R' => 'R', 'r' => 'r', "'" => '-', '"' => '-'
+        );
+        return strtr($value, $table);
     }
 }
